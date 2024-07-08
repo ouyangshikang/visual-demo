9b167fd8f1c8f9e87eac7dfdb4abbb96a9fe6d51
ouyangshikang <shikang.ouyang@lkcoffee.com>
2024-07-08 19:22:05 +0800

    feat: visual-demo 项目搭建

---
+++ demos/drawer/main.js
@@ -0,0 +1,32 @@
+import { Drawer } from './lib/drawer';
+import { DragableAndScalableRect } from './lib/rect';
+import { a } from '@vd/utils';
+
+console.log(a);
+
+// 创建画板
+const drawer = new Drawer('#drawer');
+// 创建几何图形
+const rect1 = new DragableAndScalableRect({
+  x: 500, // 中心点 x 坐标
+  y: 300, // 中心点 y 坐标
+  width: 200,
+  height: 200,
+  minWidth: 20,
+  minHeight: 20,
+  cornerWidth: 20,
+});
+drawer.addPolygon(rect1);
+
+// const rect2 = new DragableAndScalableRect({
+//   x: 100, // 中心点 x 坐标
+//   y: 100, // 中心点 y 坐标
+//   width: 100,
+//   height: 100,
+//   cornerWidth: 10,
+// });
+// drawer.addPolygon(rect2);
+
+window.onresize = () => {
+  drawer.onResize();
+};
