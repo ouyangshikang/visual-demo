eab6a197292dbaf186af3409b5539991ad12be07
ouyangshikang <shikang.ouyang@lkcoffee.com>
2024-08-07 04:10:07 +0800

    chore: init vue

--- src/components/test.vue
+++ src/components/test.vue
@@ -13,11 +13,6 @@ const props = withDefaults(defineProps<Props>(), {
   msg: 'hello'
 });
 
-interface Emits {
-  (e: 'visible', visible: boolean): void;
-}
-const emit = defineEmits<Emits>();
-
 const message = computed(() => props.msg);
 onMounted(() => {});
 </script>
