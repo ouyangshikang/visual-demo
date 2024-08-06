d40a818f3d3e0dcb3b8f2d28d490c1bbb9ddc5be
ouyangshikang <shikang.ouyang@lkcoffee.com>
2024-08-07 04:37:15 +0800

    chore: 调整打包路径

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
+  base: './',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
